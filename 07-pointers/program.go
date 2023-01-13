0636633ae0ac8789e05262c8d8074a27239e0d58
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-13 14:27:26 +0530

    updates

--- 07-pointers/program.go
+++ 07-pointers/program.go
@@ -24,8 +24,8 @@ func main() {
 
 	n1, n2 := 100, 200
 	fmt.Printf("Before swapping, n1 = %d and n2 = %d\n", n1, n2)
-	swap( /*  */ )
-	fmt.Printf("Before swapping, n1 = %d and n2 = %d\n", n1, n2)
+	swap(&n1, &n2)
+	fmt.Printf("After swapping, n1 = %d and n2 = %d\n", n1, n2)
 }
 
 func increment(x *int) {
@@ -34,6 +34,6 @@ func increment(x *int) {
 	*x++
 }
 
-func swap( /*  */ ) {
-
+func swap(x, y *int) {
+	*x, *y = *y, *x
 }
